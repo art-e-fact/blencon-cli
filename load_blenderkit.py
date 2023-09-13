305bdf63cf7b97a3cdcd96c3fd0e1b6907401294
Tomo Norman <tomo.l.norman@gmail.com>
2023-09-13 14:38:48 +0900

    move to main.py

--- load_blenderkit.py
+++ load_blenderkit.py
@@ -65,32 +65,4 @@ def convert_blender_model(model: str, model_name: str = None):
     demo_path = add_demo_world(model_name)
 
 
-def main(args):
-    convert_blender_model(args.model, args.name)
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="""
-        CLI app for converting Blender models to (Gazebo) SDF
-        Example usage:
-          python load_blenderkit.py --model model.blend --name cat-model
-        """
-    )
-
-    parser.add_argument(
-        "--model",
-        type=str,
-        required=True,
-        help="The file location of the model",
-    )
-
-    parser.add_argument(
-        "--name",
-        type=str,
-        help="The name of the model. Needs to be Unique",
-    )
-
-    args = parser.parse_args()
-
-    main(args)
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-# Use existing blender model
-# Need models directory and a foldername which is the model name
+import argparse
+from load_blenderkit import convert_blender_model
+
+def main(args):
+    convert_blender_model(args.model, args.name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""
+        CLI app for converting Blender models to (Gazebo) SDF
+        Example usage:
+          python load_blenderkit.py --model model.blend --name cat-model
+        """
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        required=True,
+        help="The file location of the model",
+    )
+
+    parser.add_argument(
+        "--name",
+        type=str,
+        help="The name of the model. Needs to be Unique",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
